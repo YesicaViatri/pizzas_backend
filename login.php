8f4e37bae0420f249b00d62db33e8fa26f961555
Yesica-Viatri <yesicaviatri@outlook.com>
2023-09-18 20:32:33 -0300

    agregado de bootstrap

--- login.php
+++ login.php
@@ -3,10 +3,9 @@ include_once("config_login.php");
 
 try{
 $pdo = new PDO("mysql:host=".SERVER_NAME.";dbname=".DATABASE_NAME,USER_NAME,PASSWORD);
-<<<<<<< HEAD
 // set the PDO error mode to exception
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-echo "conexion exitosa ";
+//echo "Conexion exitosa ";
 }
 catch(PDOException $e) {
     echo "Conexion fallida: " . $e->getMessage();
@@ -23,17 +22,30 @@ $stmt->bindValue(':hashed_pass',$hashed_pass);
 $stmt->execute();
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
 if ($row == 0) {
-    echo "Login invalido";
+    ?>
+        <div class="alert alert-danger">
+          <a href="login.html" class="close" data-dismiss="alert">×</a>
+          <div class="text-center">
+            <h5><strong>¡Error!</strong> Login Invalido.</h5>
+          </div>
+        </div>
+      <?php
+        } else {
+          echo "Bienvenido";
+        }
+        
     
-    } else {
-      echo "Bienvenido";
-    }
-    
-=======
-echo "conexion exitosa";
-}
-catch(PDOException $e) {
-    echo "Conexion fallida";
-}
->>>>>>> fd6b9c38c50b2b9145c81f0adacec9c023f5b7bc
-?>
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+   </body>
+</html>
