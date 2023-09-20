0b6da061f796574185dbebd161c00a540d419030
Yesica-Viatri <yesicaviatri@outlook.com>
2023-09-20 20:56:23 -0300

    creacion de tabla en html dento del php

--- welcome.php
+++ welcome.php
@@ -22,5 +22,8 @@ if ($_SESSION['logueado'])
 echo "Bienvenido/a ".$_SESSION['username'];
 echo "<br>";
 echo "hora de conexion ".$_SESSION['time'];
+echo "<br>";
+echo "<br>";
+$table = "<table>"."<thead>"."<tr>"."<th>Id</th>"."<th>Producto</th>"."<th>Categoria</th>"."<th>Precio</th>"."<th>Fecha de Alta</th>"."</tr>"."</thead>";
 }
 ?>
