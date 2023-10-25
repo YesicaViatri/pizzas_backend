e94a304375e1248ad11f60bc90e003f6f0d630d5
Yesica-Viatri <yesicaviatri@outlook.com>
2023-10-25 20:11:25 -0300

    cambios en welcome.php

--- welcome.php
+++ welcome.php
@@ -8,7 +8,7 @@ echo "<br>";
 echo "hora de conexion ".$_SESSION['time'];
 echo "<br>";
 echo "<br>";
-$table = "<table class='table table_bordered table-striped'><thead class='thead-dark'><tr><th>Id</th><th>Producto</th><th>Categoria</th><th>Precio</th><th>Fecha de Alta</th></tr></thead>";
+$table = "<table class='table table_bordered table-striped'><thead class='thead-dark'><tr><th>Id</th><th>Producto</th><th>Categoria</th><th>Precio</th><th>Fecha de Alta</th><th>Eliminar</th><th>Actualizar</th></tr></thead>";
 }
 ?>
 <a href="insert-products.php" class='btn btn-primary'>Ingresar Productos</a>
@@ -36,7 +36,7 @@ try{
     echo "<td>";
     echo $row['id_product'];
     echo "</td>";
-
+  
     echo "<td>";
     echo $row['product_name'];
     echo "</td>";
@@ -54,7 +54,16 @@ try{
     echo $row['start_date'];
     echo "</td>";
 
+    echo "<td>";
+    ?>
+    <a href="#" onclick="deleteProduct(<?php echo $row['id_product'] ?>)">Eliminar</a>
+    <?php
+    echo "</td>";
 
+    echo "<td>";
+    echo "<a href=''>Actualizar</a>";
+    echo "</td>";
+    
     echo "</tr>";
     }
     echo "</tbody>";
@@ -67,6 +76,14 @@ try{
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="js/jquery-3.7.1.min.js"></script>
+    <script src="js/bootbox.min.js"></script>
+    <script>
+        function deleteProduct(cod)
+        {
+            bootbox.confirm('Desea usted eliminar el producto?' +cod, function(result){if (result){window.location="delete.php?q="+cod;}});
+        }
+        </script>
     <title>Document</title>
 </head>
 <body>
